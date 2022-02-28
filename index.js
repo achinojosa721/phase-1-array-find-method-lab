fc633a2928b472f0a654003b44b4011e9464ebd4
Alberto Hinojosa <ahinojosa721@gmail.com>
2022-02-27 21:40:29 -0600

    commit tree 8a5188a8b32fdf9697f4ad81605d3fb847815323

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record){
+    const win = record.find(record => record.result === "W")
+    console.log("year", win)
+        if(!win) {
+            return win
+        }
+            return (win.year)
+    
+    
+}
